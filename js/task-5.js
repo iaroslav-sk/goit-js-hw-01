b27f9d722a9f96a052918248067f7e5ccdcc77b5
iaroslav-sk <iaroslav.dev.sk@gmail.com>
2020-11-30 23:09:19 +0200

    task-5 (fix mark)
    надалі if-else має закунчуватися else
    
    5 - далі помилка https://prnt.sc/vt2y1z
    Today at 9:12 pm

--- js/task-5.js
+++ js/task-5.js
@@ -3,30 +3,30 @@ let country = prompt('Введите страну доставки!');
 
 if (country === null) {
   country = alert(`Отменено пользователем!`);
-}
+} else {
+  switch (country.toLowerCase()) {
+    case 'китай':
+      postPrice = '100';
+      break;
+    case 'чили':
+      postPrice = '250';
+      break;
 
-switch (country.toLowerCase()) {
-  case 'китай':
-    postPrice = '100';
-    break;
-  case 'чили':
-    postPrice = '250';
-    break;
+    case 'австралия':
+      postPrice = '170';
+      break;
 
-  case 'австралия':
-    postPrice = '170';
-    break;
+    case 'индия':
+      postPrice = '80';
+      break;
 
-  case 'индия':
-    postPrice = '80';
-    break;
+    case 'ямайка':
+      postPrice = '120';
+      break;
 
-  case 'ямайка':
-    postPrice = '120';
-    break;
-
-  default:
-    alert('В вашей стране доставка не доступна!');
+    default:
+      alert('В вашей стране доставка не доступна!');
+  }
 }
 
 alert(`Стоимость достаки - ${postPrice} кредитов!`);
